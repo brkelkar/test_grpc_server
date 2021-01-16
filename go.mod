c13c72d332e6773deb19fe22d34ad1553c22742e
Balkrishna Kelkar <brkelkar@gmail.com>
2021-01-16 18:51:19 +0530

    Added configuration base litsen

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/brkelkar/test_grpc_server
 go 1.15
 
 require (
+	github.com/brkelkar/common_utils v1.0.0
 	github.com/golang/protobuf v1.4.2
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
--- main.go
+++ main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"log"
 	"net"
+	"strconv"
+
+	cr "github.com/brkelkar/common_utils/configreader"
+	"github.com/brkelkar/common_utils/logger"
 
 	"github.com/brkelkar/test_grpc_server/auth"
 	"google.golang.org/grpc"
 )
 
+var envVerables = []string{"SERVER_PORT", "SERVER_HOST", "AWACS_DB", "AWACS_SMART_DB", "AWACS_SMART_STOCKIST_DB",
+	"DB_PORT", "DB_HOST", "DB_USERNAME", "DB_PASSWORD", "GRPC_SERVER", "GRPC_CLIENT"}
+
 func main() {
-	lis, err := net.Listen("tcp", ":9000")
+
+	logger.Info("Starting Sync upload service")
+
+	var cfg cr.Config
+	//Read configuration from config.yml file
+	cfg.ReadFile("config.yml")
+
+	//Read enviroment variables
+	m := cfg.ReadEnv(envVerables)
+
+	//Over write config file variables if enviroment variable is set
+	cfg.MapEnv(m)
+	port := strconv.Itoa(cfg.GrpcConfig.Port)
+	lis, err := net.Listen("tcp", cfg.GrpcConfig.Host+":"+port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
